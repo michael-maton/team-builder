f0e8b8299710ec024db95bc5bfd6301fab0726d6
Michael Maton <mjmaton6@gmail.com>
2020-11-10 15:24:17 -0600

    MVP complete

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import "./App.css";
+import Member from "./Member";
+
+
+const initialFormValues = {
+  memberName: '',
+  email: '',
+  role: '',
+};
+
+const initialMembersList = [
+  {
+    id: "0", // uuid is a lib to generate random, unique ids
+    memberName: 'Michael',
+    email: 'michael@michael.com',
+    role: 'Student',
+  }
+];
+
+function App() {
+  const [members, setMembers] = useState(initialMembersList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues, [inputName]: inputValue
+    });
+  };
+  
+  const submitForm = () => {
+    // debugger;
+    const newMember = {
+      memberName: formValues.memberName.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    };
+
+    if (!newMember.memberName || !newMember.email || !newMember.role) return;
+    else {
+      setMembers([newMember, ...members]);
+      setFormValues(initialFormValues);
+    }
+  }
+  
+  console.log(members);
+  return (
+    <div className="container">
+      <h1>Member Form</h1>
+
+      <Form values={formValues} update={updateForm} submit={submitForm} />
+
+      {members.map((member) => {
+        return <Member key={member.id} details={member} />;
+      })}
+      {/* {members.map((member) => {
+        return(
+          <div className='friend container'>
+            <h2>{member.memberName}</h2>
+            <p>Email: {member.email}</p>
+            <p>Role: {member.role}</p>
+          </div>
+      ) 
+      })} */}
+    </div>
+  );
+}
+
+export default App;
